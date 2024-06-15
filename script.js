0ba87cee8c7770c9932926799abefe68f22b3443
staples17 <hnplaceholder@gmail.com>
2024-06-14 23:41:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var API_key = "RGAPI-10bbb9c7-9a48-4cd8-9d92-466caa1d6157";
+var summoner_name= "";
+var summoner_tag ="";
+var na_puuid_url = "https://americas.api.riotgames.com";
+
+
+function get_puuid()
+{
+    summoner_name = document.getElementById("summoner_name").value;
+    summoner_tag = document.getElementById("summoner_tag").value;
+    console.log(summoner_name);
+    console.log(summoner_tag);
+    data();
+    
+}
+
+async function data()
+{
+
+    var puuidUrl = "/riot/account/v1/accounts/by-riot-id/"+summoner_name+"/"+summoner_tag;
+    var fullSummonerNameUrl = na_puuid_url+puuidUrl+"?api_key="+API_key;
+    console.log(fullSummonerNameUrl);
+    const summonerPuuid = await fetch(fullSummonerNameUrl);
+    const summonerPuuid2 = await summonerPuuid.jsonp();
+    console.log(summonerPuuid2);
+
+    var bigpuuid = summonerPuuid2.puuid;
+    console.log(bigpuuid);
+
+    document.getElementById("puuiddisplay").innerHTML = bigpuuid;
+}
+
+
+
+
